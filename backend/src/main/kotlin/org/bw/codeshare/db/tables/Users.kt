1a810fe1e09049dfb83e1724d69919924606812a
BradleyWood <bradley.wood@uoit.net>
2018-04-29 17:13:43 -0400

    add more fields to users table

--- backend/src/main/kotlin/org/bw/codeshare/db/tables/Users.kt
+++ backend/src/main/kotlin/org/bw/codeshare/db/tables/Users.kt
@@ -4,4 +4,9 @@ import org.jetbrains.squash.definition.*
 
 object Users : TableDefinition() {
     val id = integer("id").primaryKey().autoIncrement()
+    val email = varchar("email", 254).uniqueIndex()
+    val userName = varchar("userName", 24)
+    val passwordHash = varchar("passwordHash", 64)
+    val creationDate = long("creationDate")
+    val lastLogin = long("lastLogin")
 }
