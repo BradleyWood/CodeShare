88beecb8c675bf591654c6787e59567e50f4fd8b
BradleyWood <bradley.wood@uoit.net>
2018-05-01 16:46:32 -0400

    install headers to pipeline

--- backend/src/main/kotlin/org/bw/codeshare/Application.kt
+++ backend/src/main/kotlin/org/bw/codeshare/Application.kt
@@ -2,11 +2,12 @@ package org.bw.codeshare
 
 import org.jetbrains.ktor.application.Application
 import org.jetbrains.ktor.application.install
+import org.jetbrains.ktor.features.DefaultHeaders
 import org.jetbrains.ktor.locations.Locations
 import org.jetbrains.ktor.routing.*
 
 fun Application.main() {
-
+    install(DefaultHeaders)
     install(Locations)
 
     routing {
