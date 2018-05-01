09aac79e19dd729771957d71b578ad3f064c416f
BradleyWood <bradley.wood@uoit.net>
2018-04-30 22:09:39 -0400

    test snippet creation

--- backend/src/test/kotlin/org/bw/codeshare/db/CSDatabaseTest.kt
+++ backend/src/test/kotlin/org/bw/codeshare/db/CSDatabaseTest.kt
@@ -1,5 +1,7 @@
 package org.bw.codeshare.db
 
+import org.bw.codeshare.Privacy
+import org.bw.codeshare.Snippet
 import org.bw.codeshare.User
 import org.junit.Test
 
@@ -31,5 +33,27 @@ class CSDatabaseTest {
 
         assertNull(db.getUser("abc@def.com", "password1"))
         assertNull(db.getUser("abc@def.com1", "password"))
+
+        try {
+            db.createUser(user)
+            fail("User already exists")
+        } catch (e: Exception) {
+        }
+    }
+
+    @Test
+    fun createSnippet() {
+        val user = User(5, "abc@def.com", "hi", "password")
+        db.createUser(user)
+        // for referential constraints
+
+        val snippet = Snippet(0, user.id, 0, 0, Privacy.PUBLIC, Privacy.OWNER, "Title", "Data")
+        db.createSnippet(snippet)
+
+        try {
+            db.createSnippet(snippet)
+            fail("Snippet already exists")
+        } catch (e: Exception) {
+        }
     }
 }
